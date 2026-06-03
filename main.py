f74a88ae425942907ca67c55ef10b87cd53f91c8
StruckAngel <your@email.com>
2026-06-03 16:45:07 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+thislists = ["Company", "Position", "Location", "Progress"]
+
+for x in thislists:
+    print("Enter" + " " + x + ":"+ " ")
+    x = input()
+    print (x)
+
+
+print (Company)
+
+
+    
+
+
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python .venv/bin/python main.py
